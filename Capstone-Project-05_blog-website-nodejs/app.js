7d6c170f072e6502e4960b2a1e79e7422b357fcc
theNAFIZ <nrpustcse@gmail.com>
2023-02-21 21:00:11 +0600

    Capstone 5: Challenge 2 done.

--- Capstone-Project-05_blog-website-nodejs/app.js
+++ Capstone-Project-05_blog-website-nodejs/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {startingContent: homeStartingContent});
 });
 
 
