af08f7f60e9e15009775326fc74d9172a7e9888a
theNAFIZ <nrpustcse@gmail.com>
2023-02-24 20:58:43 +0600

    Capstone 5: Challenge 16 done.

--- Capstone-Project-05_blog-website-nodejs/app.js
+++ Capstone-Project-05_blog-website-nodejs/app.js
@@ -41,7 +41,9 @@ app.post("/compose", (req, res) => {
   res.redirect("/");
 });
 
-
+app.get("/posts/:title", (req, res) => {
+  console.log(req.params.title);
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
